57f65e307d0b82fb2d247c47addf09126f08d551
your name <exam@mail.com>
2021-04-11 20:12:10 +0530

    Code to handle multiple ride as array

---
+++ src/main/java/Ride.java
@@ -0,0 +1,9 @@
+public class Ride {
+    public double distance;
+    public double time;
+
+    public Ride(double distance, double time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
