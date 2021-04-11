52a461345a1a4c8af800502d54daeead97c31e73
your name <exam@mail.com>
2021-04-11 19:47:24 +0530

    Code to check total fare is greater than minimum fare

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,11 +1,17 @@
 public class InvoiceGenerator {
 
-
+    private static final double COST_PER_MINUTE = 1.0 ;
+    private static final double MINIMUM_FARE = 5.0;
     public final double COST_PER_KM =10.0;
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance*COST_PER_KM+time;
-        return totalFare;
 
+        public double calculateFare(double distance, double time) {
+            double totalFare = (distance*COST_PER_KM)+(time*COST_PER_MINUTE);
+            if (totalFare<MINIMUM_FARE)
+                return MINIMUM_FARE;
+
+        return totalFare;
     }
 }
